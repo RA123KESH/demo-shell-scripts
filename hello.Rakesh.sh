d76eb09656714c9714d6088b20919274ce28b920
Rakesh <Kumarrakesh8287@gmail.com>
2025-02-28 05:38:01 +0000

    added hello.Rakesh.sh

---
+++ hello.Rakesh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rakesh: Adarniya sabhaapati mahodya"
+echo "Rakesh: aaj hum sab mil ke chamatkar krenge"
+echo "Raja: ratta marega toh aisa hi hoga"
+echo "Rakesh: thuchuk thuchuk"
